234fa336e5f9f3106c230c3d0475ce2b434a68bb
Chia <yangchai2450@gmail.com>
2024-02-01 22:57:00 -0500

    updated the codes on the database.js

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # browser-text-editor-application
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]
+
+## Description
+    
+   This simple text editor application is created to demonstrate developer's understanding of a PWA. The application also features methods that are useful for storing and retrieving data.
+
+## Installation
+    
+    The user will be able to install the app through its website
+
+ ## Usage
+
+    User will be able to edit text, and user's input will be save onto the application's native storage
+
+## Website URL
+
+    https://drive.google.com/file/d/1fdpgdBmAg3graw488Ru5VkZM70ammVTW/view
+
+
+## License
+
+    This project is coverd under the MIT license.
--- client/src/js/database.js
+++ client/src/js/database.js
@@ -15,16 +15,16 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   // console.error('putDb not implemented');
-  const contactDb = await openDB('jate', 1);
+  const jateDb = await openDB('jate', 1);
   // Create a new transaction and specify the database and data privileges.
-  const tx = contactDb.transaction('jate', 'readwrite');
+  const tx = jateDb.transaction('jate', 'readwrite');
   // Open up the desired object store.
-    const store = tx.objectStore('jate');
+  const store = tx.objectStore('jate');
   // Use the .add() method on the store and pass in the content.
-  const request = store.add(content);
+  const request = store.put({ jate: content });
   // Get confirmation of the request.
   const result = await request;
-  console.log('Data saved to the database', result);
+  console.log('Data saved to the jate database', result);
 };
 
 // TODO: Add logic for a method that gets all the content from the database
@@ -32,10 +32,10 @@ export const getDb = async () => {
   console.log('GET from the database');
 
   // Create a connection to the database database and version we want to use.
-  const contactDb = await openDB('jate', 1);
+  const jateDb = await openDB('jate', 1);
 
   // Create a new transaction and specify the database and data privileges.
-  const tx = contactDb.transaction('jate', 'readonly');
+  const tx = jateDb.transaction('jate', 'readonly');
 
   // Open up the desired object store.
   const store = tx.objectStore('jate');
